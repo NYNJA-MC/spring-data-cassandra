ba116aa70796df39313989122e0dc4b6a11c7648
zahari <zahari@softavail.com>
2020-11-09 16:36:01 +0200

    Added new method getActions().

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/config/KeyspaceActions.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/config/KeyspaceActions.java
@@ -40,4 +40,8 @@ public class KeyspaceActions {
 	public KeyspaceActions(List<KeyspaceActionSpecification> actions) {
 		this.actions = actions;
 	}
+
+	public List<KeyspaceActionSpecification> getActions() {
+		return this.actions;
+	}
 }
